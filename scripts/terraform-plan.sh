0bddee026f339d2eae11224a5416d9fc20d66337
Ben Sinclair <bensiclair@microsoft.com>
2023-03-03 00:21:10 +0000

    Apply script

---
+++ scripts/terraform-plan.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo -e "\n\e[34m╔══════════════════════════════════╗"
+echo -e "║\e[32m            Terraform \e[34m        ║"
+echo -e "║\e[33m          Plan Script  \e[34m       ║"
+echo -e "╚══════════════════════════════════╝"
+echo -e "\e[35m   v0.0.1    🚀  🚀  🚀\n"
+
+set -euo pipefail
+
+source ./load-env.sh
+
+if [[ $# -lt 1 ]]; then
+  echo -e "\e[31m»»» 💥 Pass plan name as parameter to this script e.g. $0 foo"
+  exit 1
+fi
+
+TFPLAN_NAME=${1}
+
+terraform plan -input=false -out ${TFPLAN_NAME}.tfplan

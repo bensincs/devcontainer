e99df12438111b65e564f77ef9fc36d9e5bbaff3
Ben Sinclair <bensiclair@microsoft.com>
2023-03-06 16:20:58 +0000

    Rdp tunnels

--- scripts/rdp-tunnels.sh
+++ scripts/rdp-tunnels.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-figlet RDP Socat Time
+figlet RDP Socat Tunneling
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $SCRIPT_DIR/load-env.sh
+
+IFS=', ' read -r -a array <<< "$SOCAT_RDP_REMOTE"
+
+for index in "${!array[@]}"
+do
+    port=$((8000 + $index))
+    socat tcp-listen:$port,reuseaddr,fork tcp:${array[index]} &
+done

4915f90b89d42df2947a6cba6075444d300a0334
Ben Sinclair <bensiclair@microsoft.com>
2023-03-13 10:41:50 +0000

    Allow rdp to fail

---
+++ README.md
@@ -0,0 +1,10 @@
+## Dev Container
+
+## Dev Infrastucture
+### Network
+### VPN Gateway
+### DNS Resolver
+## Scripts
+### VPN Clinet
+### DNS Cofig
+### VPN
--- scripts/rdp-tunnels.sh
+++ scripts/rdp-tunnels.sh
@@ -3,10 +3,12 @@ figlet VPN Client Create
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $SCRIPT_DIR/common.sh
 source $SCRIPT_DIR/load-env.sh
-set -e
+
 
 IFS=',' read -r -a array <<< "$SOCAT_RDP_REMOTES"
+set +e
 pkill -f socat
+set -e
 for index in "${!array[@]}"
 do
     port=$((5000 + $index))
